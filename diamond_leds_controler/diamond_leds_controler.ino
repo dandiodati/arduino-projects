728e4de0d9ba304fe9819672ba4d86bae7689f12
Dan Diodati <dan.diodati@sap.com>
2018-01-23 13:21:00 -0800

    first commit

---
+++ diamond_leds_controler/diamond_leds_controler.ino
@@ -0,0 +1,38 @@
+#include <bitswap.h>
+#include <chipsets.h>
+#include <color.h>
+#include <colorpalettes.h>
+#include <colorutils.h>
+#include <controller.h>
+#include <cpp_compat.h>
+#include <dmx.h>
+#include <FastLED.h>
+#include <fastled_config.h>
+#include <fastled_delay.h>
+#include <fastled_progmem.h>
+#include <fastpin.h>
+#include <fastspi.h>
+#include <fastspi_bitbang.h>
+#include <fastspi_dma.h>
+#include <fastspi_nop.h>
+#include <fastspi_ref.h>
+#include <fastspi_types.h>
+#include <hsv2rgb.h>
+#include <led_sysdefs.h>
+#include <lib8tion.h>
+#include <noise.h>
+#include <pixelset.h>
+#include <pixeltypes.h>
+#include <platforms.h>
+#include <power_mgt.h>
+
+void setup() {
+  // put your setup code here, to run once:
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
