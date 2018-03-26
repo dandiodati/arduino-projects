0682526106a706719d795013b0743ace1b94957f
Dan Diodati <dan.diodati@sap.com>
2018-03-26 11:57:19 -0700

    Updated to different pin since might if burnout pin 5 :-(

--- diamond_leds_controler/diamond_leds_controler.ino
+++ diamond_leds_controler/diamond_leds_controler.ino
@@ -52,10 +52,10 @@ const unsigned int hubPort = 39500;           // smartthings hub port
 
 
 // How many leds are in the strip?
-#define NUM_LEDS 23
+#define NUM_LEDS 24
 
 // Data pin that led data will be written out over
-#define DATA_PIN 5
+#define DATA_PIN 3
 
 // This is an array of leds.  One item for each led in your strip.
 CRGB leds[NUM_LEDS];
